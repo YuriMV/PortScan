cbf6da7204ab6639ca3694f7b84482e09c800c02
Yuri V. Santos <costavold145@outlook.com>
2017-03-12 04:18:00 -0300

    Create portscan.sh
    Simples portscan em shellscript

---
+++ portscan.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#coded by voldete
+#TFaction s2
+VERMELHO='\e[0;31m'
+SEMCOR='\e[0m'
+VERDE='\e[0;32m'
+PMAX=1000
+ read -p "host: " h
+for (( COUNT=21;COUNT<$PMAX;COUNT++ ))
+do
+t=`nc -z -v $h $COUNT -w 1 2>&1 | sed 's/\s.*$//'`
+ if [ "$t" = "Connection" ]
+then
+  printf "${VERDE}[+]Connected to the port: $COUNT\n${SEMCOR}"
+elif [ "$t" = "nc:" ]
+ then
+ printf "${VERMELHO}[-]Connection failure on port: $COUNT\n${SEMCOR}"
+  else
+ echo '...'
+ fi
+done
+
+ 
